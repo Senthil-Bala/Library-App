968a5eebc038048d01ba208e8c62f54c84a3bf34
Senthil-Bala <senthilkumarbala100@gmail.com>
2024-02-14 16:53:24 +0530

    changes made

--- src/Components/FetchApi/DisplayData.jsx
+++ src/Components/FetchApi/DisplayData.jsx
@@ -21,7 +21,6 @@ function DisplayData({ infos }) {
         setShowModal(true);
       });
   };
-
   const handleCloseModal = () => {
     setShowModal(false);
   };
@@ -31,32 +30,32 @@ function DisplayData({ infos }) {
       <Container fluid>
         <Row>
           {infos.map((ele, idx) => (
-            <Col lg={2} md={4} sm={6} xs={12} key={ele.work.cover_id}>
+            <Col lg={2} md={4} sm={6} xs={12} key={ele?.key}>
               <Card
                 style={{
                   minHeight: "590px",
                   width: "200px",
                   marginBottom: "2rem",
-                  borderRadius:"1rem"
+                  borderRadius: "1rem",
                 }}
                 className="text-center"
               >
                 {/* <Card.Img
                   variant="top"
-                  src={`https://covers.openlibrary.org/b/id/${ele.work.cover_id}-L.jpg`}
+                  src={https://covers.openlibrary.org/b/id/${ele.work.cover_id}-L.jpg}
                 /> */}
 
                 <Card.Body>
-                  <Card.Title className="fw-bold mb-4">{ele.work.title}</Card.Title>
+                  <Card.Title className="fw-bold mb-4">{ele?.title}</Card.Title>
                   <Card.Text
                     className="authorName"
                     onClick={() => {
-                      handleAuthor(ele.work.author_names[0]);
+                      handleAuthor(ele?.author_name[0]);
                     }}
                   >
-                    By : {ele.work.author_names[0]}
+                    By : {ele?.author_name[0]}
                   </Card.Text>
-                  <Card.Text>  On: {ele.work.first_publish_year}</Card.Text>
+                  <Card.Text> On: {ele?.first_publish_year}</Card.Text>
                   {/* <Card.Text>On : {ele.logged_date}</Card.Text> */}
                 </Card.Body>
               </Card>
@@ -68,27 +67,27 @@ function DisplayData({ infos }) {
       {/* Author Modal */}
       <Modal show={showModal} onHide={handleCloseModal} className="text-center">
         <Modal.Header closeButton>
-          <Modal.Title>{authorData.name}</Modal.Title>
+          <Modal.Title>{authorData?.name}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {authorData.length > 0 && (
+          {authorData?.length > 0 && (
             <>
               {/* <img
-                src={`https://covers.openlibrary.org/b/id/${authorData[0].cover_i}-L.jpg`}
+                src={https://covers.openlibrary.org/b/id/${authorData[0].cover_i}-L.jpg}
                 alt="Author Cover"
                 style={{ maxWidth: "100%", height: "auto" }}
               /> */}
               <p>
-                <b>Name:</b> {authorData[0].name}
+                <b>Name:</b> {authorData[0]?.name}
               </p>
               <p>
-                <b>Top Subjects</b>: {authorData[0].top_subjects.join(", ")}
+                <b>Top Subjects</b>: {authorData[0]?.top_subjects.join(", ")}
               </p>
               <p>
-                <b>Top Work</b>: {authorData[0].top_work}
+                <b>Top Work</b>: {authorData[0]?.top_work}
               </p>
               <p>
-                <b>Work Count</b>: {authorData[0].work_count}
+                <b>Work Count</b>: {authorData[0]?.work_count}
               </p>
             </>
           )}
--- src/Components/FetchApi/Fetching.jsx
+++ src/Components/FetchApi/Fetching.jsx
@@ -9,16 +9,17 @@ function Fetching() {
   const [loading, setLoading] = useState(false);
   const [infos, setInfos] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [cardsPerPage] = useState(6);
-  const [filteredInfos, setFilteredInfos] = useState([]);
+  const [cardsPerPage] = useState(30);
 
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`https://openlibrary.org/search.json?q=${searchVal}&page=${currentPage}&limit=${cardsPerPage}`)
+      .get(
+        `https://openlibrary.org/search.json?q=${searchVal}&fields=key,title,author_name,first_publish_year&page=${currentPage}&limit=${cardsPerPage}`
+      )
       .then((res) => {
         setInfos(res.data.docs);
-        setFilteredInfos(res.data.docs);
+        // setFilteredInfos(res.data.docs);
         setLoading(false);
       })
       .catch((e) => {
@@ -29,36 +30,47 @@ function Fetching() {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       handleSearch();
-    }, 500); 
+    }, 500);
 
     return () => clearTimeout(timeoutId);
   }, [searchVal]);
 
+  const handlePrevious=()=>{
+    setCurrentPage(currentPage-1)
+  }
+
+  const handleNext=()=>{
+    setCurrentPage(currentPage+1)
+  }
+
   const handleSearch = () => {
-    if (searchVal.trim() === "") {
-      setFilteredInfos(infos);
-    } else {
+   
       axios
-        .get(`https://openlibrary.org/search.json?q=${searchVal}&page=1&limit=${cardsPerPage}`)
+        .get(
+          `https://openlibrary.org/search.json?q=${searchVal}&fields=key,title,author_name,first_publish_year&page=1&limit=${cardsPerPage}`
+        )
         .then((res) => {
           setInfos(res.data.docs);
-          setFilteredInfos(res.data.docs);
+          // setFilteredInfos(res.data.docs);
         })
         .catch((e) => {
           console.log(e);
         });
-    }
+    
   };
 
-  const paginate = (pageNumber) => {
-    if (pageNumber >= 1 && pageNumber <= Math.ceil(filteredInfos.length / cardsPerPage)) {
-      setCurrentPage(pageNumber);
-    }
-  };
+  // const paginate = (pageNumber) => {
+  //   if (
+  //     pageNumber >= 1 &&
+  //     pageNumber <= Math.ceil(filteredInfos.length / cardsPerPage)
+  //   ) {
+  //     setCurrentPage(pageNumber);
+  //   }
+  // };
 
-  const lastIndex = currentPage * cardsPerPage;
-  const firstIndex = lastIndex - cardsPerPage;
-  const currentPosts = filteredInfos.slice(firstIndex, lastIndex);
+  // const lastIndex = currentPage * cardsPerPage;
+  // const firstIndex = lastIndex - cardsPerPage;
+  // const currentPosts = filteredInfos.slice(firstIndex, lastIndex);
 
   return (
     <div className="text-center p-4">
@@ -84,10 +96,13 @@ function Fetching() {
         Go
       </button>
       {loading && <p>Loading...</p>}
-      {searchVal && filteredInfos.length === 0 && (
+      {/* {searchVal && filteredInfos.length === 0 && (
         <p className="mt-2 text-danger fw-bolder">No books found !</p>
-      )}
-      <DisplayData infos={currentPosts} />
+      )} */}
+      <DisplayData infos={infos} />
+      <button className="rounded-1 p-2 bg-primary text-white mx-2" onClick={handlePrevious}>Previous</button>
+      <button className="rounded-1 p-2 bg-primary text-white mx-2" >{currentPage}</button>
+      <button className="rounded-1 p-2 bg-primary text-white mx-2" onClick={handleNext}>Next</button>
       {/* <Pages
         totalpost={filteredInfos.length}
         paginate={paginate}
